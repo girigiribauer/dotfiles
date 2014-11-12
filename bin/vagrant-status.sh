cc65151eb40303378ba7d01a01cce71136a168ec
girigiribauer <girigiribauer@gmail.com>
2014-11-12 22:59:10 +0900

    tmux に合わせて半角スペースあけた

--- bin/vagrant-status.sh
+++ bin/vagrant-status.sh
@@ -3,4 +3,4 @@
 # VM が立ち上がっているかどうかチェック
 cd ~/Vagrant
 display=`vagrant status`
-echo -e "$display" | grep "running" | sed 's/\([^ ]*\)  *\(running.*$\)/[\1:ON]/' | paste -s --delimiters=''
+echo -e "$display" | grep "running" | sed 's/\([^ ]*\)  *\(running.*$\)/[\1:ON]/' | paste -s --delimiters=' '
