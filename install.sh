18d1215ab363985430106eb75beed2fcf9cb7160
girigiribauer <girigiribauer@gmail.com>
2015-05-21 16:59:43 +0900

    シンボリックリンクが同じリンク元だったら無視

--- install.sh
+++ install.sh
@@ -2,16 +2,13 @@
 
 path=$(cd $(dirname $0) && pwd)
 
-
 function symlinks() {
   local name=$1
-  if [ -e "$HOME/$name" ]
+  if test $(readlink -f "$path/${name}") != $(readlink -f "$HOME/${name}")
   then
-    echo "mv $HOME/$name $HOME/${name}_backup --backup=t"
-    mv $HOME/$name $HOME/${name}_backup --backup=t
+    echo "ln -s $path/$name $HOME/$name --backup=t"
+    ln -s $path/$name $HOME/$name --backup=t
   fi
-  echo "ln -s $path/$name $HOME/$name"
-  ln -s $path/$name $HOME/$name
 }
 
 symlinks ".vimrc"
