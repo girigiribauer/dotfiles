401a8e60aabca3b80a518cf9294d16adc22e25a3
girigiribauer <girigiribauer@gmail.com>
2016-09-03 18:57:59 +0900

    Docker が反応していない場合も追加

--- bin/vm_status.sh
+++ bin/vm_status.sh
@@ -10,7 +10,7 @@ vagrant=`echo -e $vagrant_statuses | grep "running" | awk '{ printf " [" $1 ":ON
 # ソケットファイルを通じて docker サービスが稼働中か調べる
 docker_services=`curl --silent --unix-socket /var/run/docker.sock http:/info | tr -d "\n"`
 docker=''
-if [ "$docker_services" != "" ]; then
+if [ "$docker_services" != "" || "$docker_services" != "Bad response for Docker engine" ]; then
   docker='[docker:ON]'
 fi
 
