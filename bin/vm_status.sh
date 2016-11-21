78ad3c274f9eaaf9e7726234c923d1d90cbc55bc
girigiribauer <girigiribauer@gmail.com>
2016-11-21 17:51:40 +0900

    update status line vm

--- bin/vm_status.sh
+++ bin/vm_status.sh
@@ -8,11 +8,11 @@ vagrant_statuses=`cat $vagrant_configfile_raw | jq "$jq_filter" --raw-output`
 vagrant=`echo -e $vagrant_statuses | grep "running" | awk '{ printf " [" $1 ":ON]" }'`
 
 # ソケットファイルを通じて docker サービスが稼働中か調べる
-docker_services=`curl --silent --unix-socket /var/run/docker.sock http:/info | tr -d "\n"`
-docker=''
-if [ "$docker_services" != "" || "$docker_services" != "Bad response for Docker engine" ]; then
-  docker='[docker:ON]'
-fi
+#docker_services=`curl --silent --unix-socket /var/run/docker.sock http:/info | tr -d "\n"`
+#docker=''
+#if [ "$docker_services" != "" || "$docker_services" != "Bad response for Docker engine" ]; then
+#  docker='[docker:ON]'
+#fi
 
 # xargs as trailing spaces
 echo "$docker $vagrant" | xargs
