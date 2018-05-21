eb305b21b3f863a53c7b6ae5599ed27367e751c0
girigiribauer <girigiribauer@gmail.com>
2018-05-21 17:47:33 +0900

    config.fish

--- config.fish
+++ config.fish
@@ -7,7 +7,7 @@ set -xg LANG "en_US.UTF-8"
 set -xg LC_ALL "en_US.UTF-8"
 
 # shell
-#set -xg SHELL "/usr/local/bin/fish"
+set -xg SHELL "/usr/local/bin/fish"
 
 # for XDG base directory specification
 set -xg XDG_CONFIG_HOME "$HOME/.config"
@@ -16,6 +16,13 @@ set -xg XDG_DATA_HOME "$HOME/.local/share"
 
 # no message
 set fish_greeting
+
+# ls colors
+set -xg LSCOLORS "gxfxbEaEBxxEhEhBaDaCaD"
+
+# golang
+set -xg GOPATH "$HOME/go"
+set -x PATH $PATH "$GOPATH/bin"
 
 
 
@@ -44,48 +51,52 @@ end
 # mapping settings
 # ---------------------------------------------------------------------------- #
 
+if status --is-interactive
+  set -g fish_user_abbreviations
+
 # gui
-alias o "open ."
+  abbr -a o "open ."
 
 # vim
-alias vi "nvim"
-alias vim "nvim"
-alias nvim "nvim -p"
+  alias vi "nvim"
+  alias vim "nvim"
+  alias nvim "nvim -p"
 
 # git
-alias g "git"
-alias ga "git add"
-alias gaa "git add ."
-alias gb "git branch --all"
-alias gbd "git branch -d "
-alias gc "git commit"
-alias gca "git commit -a"
-alias gco "git checkout"
-alias gcom "git checkout master"
-alias gcod "git checkout develop"
-alias gcob "git checkout -b"
-alias gre "git rebase -i"
-alias gd "git diff"
-alias gl "git log --graph --all --pretty=format:'%Cred%h%Creset %Cgreen(%cI) -%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset' --abbrev-commit --date=rfc2822"
-alias gp "git pull"
-alias gs "git status"
-alias gst "git stash"
-alias gf "git fetch"
+  abbr -a g "git"
+  abbr -a ga "git add"
+  abbr -a gaa "git add ."
+  abbr -a gb "git branch --all"
+  abbr -a gbd "git branch -d "
+  abbr -a gc "git commit"
+  abbr -a gca "git commit -a"
+  abbr -a gco "git checkout"
+  abbr -a gcom "git checkout master"
+  abbr -a gcod "git checkout develop"
+  abbr -a gcob "git checkout -b"
+  abbr -a gre "git rebase -i"
+  abbr -a gd "git diff"
+  abbr -a gl "git log --graph --all --pretty=format:'%Cred%h%Creset %Cgreen(%cI) -%C(yellow)%d%Creset %s %C(bold blue)<%an>%Creset' --abbrev-commit --date=rfc2822"
+  abbr -a gp "git pull"
+  abbr -a gs "git status"
+  abbr -a gst "git stash"
+  abbr -a gf "git fetch"
 
 # docker
-alias d "docker"
-alias dc "docker container" # override original dc command
-alias dls 'docker container ls'
-alias di "docker image"
-alias dils "docker image ls"
-alias dn "docker network"
-alias dnls "docker network ls"
-alias dv "docker volume"
-alias dvls "docker volume ls"
-alias dcom "docker-compose"
-alias drun "docker run"
-alias dex "docker exec"
-alias dpull "docker pull"
+  abbr -a d "docker"
+  abbr -a dc "docker container" # override original dc command
+  abbr -a dls 'docker container ls'
+  abbr -a di "docker image"
+  abbr -a dils "docker image ls"
+  abbr -a dn "docker network"
+  abbr -a dnls "docker network ls"
+  abbr -a dv "docker volume"
+  abbr -a dvls "docker volume ls"
+  abbr -a dcom "docker-compose"
+  abbr -a drun "docker run"
+  abbr -a dex "docker exec"
+  abbr -a dpull "docker pull"
+end
 
 
 
